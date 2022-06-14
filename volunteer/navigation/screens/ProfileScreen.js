4a5d81695243be9808037a9e55005ffd5dd409af
dwang88 <davidwang1089@gmail.com>
2022-06-14 11:10:26 -0700

    initial commit

---
+++ volunteer/navigation/screens/ProfileScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ProfileScreen({navigation}) {
+    return(
+        <View>
+            <Text>
+                
+                </Text>
+        </View>
+    );
+}
+
---
+++ volunteer/navigation/screens/SettingsScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SettingsScreen({navigation}) {
+    return(
+        <View>
+            <Text>
+                </Text>
+        </View>
+    );
+}
+
